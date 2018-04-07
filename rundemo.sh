4304116abcd445f9be6c725adefd416b1f8ce543
Konrad Aust <konrad.aust@gmail.com>
2018-04-07 19:35:58 +0000

    Added write/print readme casts to demo

--- rundemo.sh
+++ rundemo.sh
@@ -1,5 +1,9 @@
 while true
 do
+  asciinema play -i 2 write-readme.cast
+  asciinema play -i 2 print-readme.cast
+  sleep 3
+  
   for f in /home/pi/fine-quines/casts/*.cast
   do
     asciinema play -i 2 $f
